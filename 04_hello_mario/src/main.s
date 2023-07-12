6b815e5f15d34fd72f42a434e5c7bd9e2991035e
Riley Fischer <rfmineguy@gmail.com>
2023-07-12 16:52:50 -0700

    Zero out PPUCTROL/PPUMASK

--- 04_hello_mario/src/main.s
+++ 04_hello_mario/src/main.s
@@ -8,12 +8,26 @@
 .segment "ZEROPAGE"
 .segment "STARTUP"
 reset:
+    sei             ;disable interrupts
+    cld             ;disable decimal mode (NES 6502 doesn't support it)
+    ldx #$40
+    stx $4017       ;disable sound irq
+    stx #$ff
+    tsx             ;initialize the stack to 0xff
+    inx             ;ff + 1 = 0
+.zeroPPU:
+    stx $2000       ;PPUCTRL = 0
+    stx $2001       ;PPUMASK = 0
+    stx $4010       ;...
+
+loop:
+    jmp loop
 
 nmi:
+    rti
 
 .segment "VECTORS"
-.word nmi
-.word reset
-.word 0
+.word nmi reset
+
 .segment "CHARS"
 .incbin "hellomario.chr"

0406e8ba7bf8c890e12a9681ea3424fe50ed4bcc
Riley Fischer <rfmineguy@gmail.com>
2023-07-12 17:38:42 -0700

    Made the make script a little nicer

--- 04_hello_mario/make.bat
+++ 04_hello_mario/make.bat
@@ -1,5 +1,2 @@
-ca65 main.s -o main.o --debug-info
-ld65 main.o -o main.nes -t nes --dbgfile main.dbg
-move main.o out
-move main.nes out
-move main.dbg out
+ca65 src/main.s -o out/main.o --debug-info
+ld65 out/main.o -o out/main.nes -t nes --dbgfile out/main.dbg

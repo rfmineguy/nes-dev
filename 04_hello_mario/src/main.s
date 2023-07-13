55501704d0f5819bebe5d886fc23769321b1860e
Riley Fischer <rfmineguy@gmail.com>
2023-07-12 17:03:09 -0700

    Clear RAM but set aside space for sprite data at /usr/bin/bash200

--- 04_hello_mario/src/main.s
+++ 04_hello_mario/src/main.s
@@ -19,7 +19,24 @@ reset:
     stx $2000       ;PPUCTRL = 0
     stx $2001       ;PPUMASK = 0
     stx $4010       ;...
-
+    :
+    bit $2002       ;test bit 7 (vblank ready)
+    bpl -           ;branch if not negative (negative if bit 7 is 1)
+    txa             ;A=X
+.clearMem:
+    sta $0000, X    ;$0000->$00FF   set to 0
+    sta $0100, X    ;$0100->$01FF   set to 0
+    ;sta $0200, X   ;$0200->$02FF
+    sta $0300, X    ;$0300->$03FF   set to 0
+    sta $0400, X    ;$0400->$04FF   set to 0
+    sta $0500, X    ;$0500->$05FF   set to 0
+    sta $0600, X    ;$0600->$06FF   set to 0
+    sta $0700, X    ;$0700->$07FF   set to 0
+    lda #$ff
+    sta $0200, X    ;$0200->$02FF   set to FF (set aside for sprite data)
+    lda #$00
+    inx
+    bne .clearMem   ;if x rolled over to 0 move on
 loop:
     jmp loop
 
